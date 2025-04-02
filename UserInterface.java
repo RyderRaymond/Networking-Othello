4c53404b747881e5c72bb70671240274955d1a95
rw555 <157328874+rw555@users.noreply.github.com>
2025-04-02 11:18:37 -0400

    add comment

--- UserInterface.java
+++ UserInterface.java
@@ -1,3 +1,9 @@
+/*
+ * Class UserInterface
+ * 
+ * This class allows the player to interact with the othello game by displaying the board
+ * and allowing for an input of coordinates for moves.
+ */
 public class UserInterface {
     private int boardState[][]; // 2d array to hold the board state that will be displayed to the player
 }
