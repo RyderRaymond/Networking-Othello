4a676ccbb9c629f20b3cd8d242e2a89a063bb004
Ryder Raymond <117836603+RyderRaymond@users.noreply.github.com>
2025-04-15 10:45:45 -0400

    Add scaffolding for Server.java

--- Server.java
+++ Server.java
@@ -1,4 +1,29 @@
+import java.util.ArrayList;
+
 public class Server {
     private Color[][] board = new Color[8][8];
     private final Color serverColor = Color.AI;
+
+    private void get_player_move() {
+
+    }
+
+    private int[][] check_updated_coordinates() {
+      return new int[][] {};
+    }
+
+    private void send_client_updated_moves() {
+
+    }
+
+    private void make_server_move() {
+    }
+
+    private int[][] get_server_updated_coords() {
+      return new int[][] {};
+    }
+
+    private void send_update_to_client() {
+
+    }
 }
