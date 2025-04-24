513593933bdd0fcc1f83e30692822f6954fb258b
Ryder Raymond <117836603+RyderRaymond@users.noreply.github.com>
2025-04-24 16:59:31 -0400

    Working on server

--- Server.java
+++ Server.java
@@ -1,6 +1,7 @@
 import java.io.*;
 import java.net.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Server {
@@ -34,7 +35,7 @@ public class Server {
                     newThread.start();
                 }
                 catch (Exception ex) {
-                    System.out.println("Error creating new thread for client: " + ex.getMessage())
+                    System.out.println("Error creating new thread for client: " + ex.getMessage());
                 }
             }
         }
@@ -42,12 +43,11 @@ public class Server {
             System.out.println("There was an error while running the server: " + ex.getMessage());
         }
     }
-
-
-
 }
 
 class ServerThread extends Thread {
+    private static final int sleepTimeAfterSendUpdate = 5;
+
     private final Color aiColor = Color.AI;
     private OthelloPlayer aiPlayer;
     private BufferedReader reader;
@@ -63,14 +63,40 @@ class ServerThread extends Thread {
 
     public void run() {
         int[] lastMove = null;
-        int[] playerCoord = getPlayerMove();
+        int[] playerCoord = null;
 
-        // Check if last move was pass and player move is pass then game is over
+        do {
+            playerCoord = getPlayerMove();
 
-        ArrayList<int[]> playerUpdatedCoords = OthelloPlayer.place(aiPlayer.getBoard(), playerCoord, aiColor);
-        aiPlayer.updateBoard(playerUpdatedCoords);
+            // Check if last move was pass and player move is pass then game is over
+            if (Arrays.equals(lastMove, new int[]{-1, 0}) && Arrays.equals(playerCoord, new int[]{1, 0})) {
+                // end game logic
+                Color winner = OthelloPlayer.winner(aiPlayer.getBoard());
+                // Need to meet on if the server sends the winner to the player or if both get it independently
+            }
 
-        // Send playerUpdatedCoords to player
+            ArrayList<int[]> playerUpdatedCoords = OthelloPlayer.place(aiPlayer.getBoard(), playerCoord, Color.PLAYER);
+            aiPlayer.updateBoard(playerUpdatedCoords);
+
+            // Send playerUpdatedCoords to player
+            sendClientUpdatedCoords(playerUpdatedCoords);
+
+            try {
+                Thread.sleep(sleepTimeAfterSendUpdate);
+            }
+            catch (InterruptedException ex) {
+                System.out.println("Problem sleeping to let the user look at what coordinates are updated: " + ex.getMessage() + ".\nContinuing execution.");
+            }
+
+            // AI Logic to get the best move and call it on that
+            int[]  serverMove = makeServerMove();
+
+            ArrayList<int[]> serverUpdatedCoords = OthelloPlayer.place(aiPlayer.getBoard(), serverMove, Color.AI);
+
+
+
+
+        } while (lastMove == null || !(Arrays.equals(lastMove, new int[]{-1, 0}) && Arrays.equals(playerCoord, new int[]{1, 0})));
     }
 
     //Client should send data as "coord, coord"
@@ -97,22 +123,17 @@ class ServerThread extends Thread {
         return playerMove;
     }
 
-    private int[][] checkUpdatedCoordinates() {
-        return new int[][] {};
-    }
 
-    private void sendClientUpdatedMoves() {
+    private void sendClientUpdatedCoords(ArrayList<int[]> coords) {
 
     }
 
-    private void makeServerMove() {
+    private int[] makeServerMove() {
+        return new int[] {};
     }
 
-    private int[][] getServerUpdatedCoords() {
-        return new int[][] {};
-    }
 
-    private void sendUpdateToClient() {
+    private void sendServerUpdatedCoords() {
 
     }
 }
