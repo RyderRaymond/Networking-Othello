3cc6644600351c4645b47e1f78369d830f661c4c
Ryder Raymond <117836603+RyderRaymond@users.noreply.github.com>
2025-04-27 23:59:33 -0400

    Add basic framework and networking methods
    I'm writing what I did here so I don't forget
    
    1) Changed client so that it makes it's own object
    2) Added networking methods. One that returns an int[][] needs to be
       changed to return the ArrayList<int[]> and then we need logic to
       parse this into in[][] for UI.
    
    Notes:
    - it looks like the coordinates are reversed, like 5, 3 places at x=3
      and y=5
    - AI always says there are no valid AI moves despite having moves
    - I don't know how to get the input from the UI. It says submitting but
      then we need an event listener or semaphore that the Client can wait
      to change to the input.

--- Client.java
+++ Client.java
@@ -1,11 +1,12 @@
 import java.io.*;
 import java.net.*;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Client {
 
     public static void main(String[] args) {
-        try{
+        try {
             String host = "localhost";
             int port = 9999;
 
@@ -13,18 +14,161 @@ public class Client {
             BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
             BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
+            new ClientPlayer(socket, writer, reader).play();
+
+
+            reader.readLine();
+
+
+        }
+        catch (IOException ex) {
+            System.out.println("Error connecting to host: " + ex.getMessage());
+        }
+    }
+
+
+
+}
+
+class ClientPlayer {
+    private Socket socket;
+    private BufferedWriter writer;
+    private BufferedReader reader;
+    private OthelloPlayer othelloPlayer;
+
+    public ClientPlayer(Socket socket, BufferedWriter writer, BufferedReader reader)
+    {
+        this.socket = socket;
+        this.writer = writer;
+        this.reader = reader;
+        othelloPlayer = new OthelloPlayer(Color.PLAYER);
+    }
+
+    public void play() {
+        try {
+            int[] lastMove = null;
             UserInterface ui = new UserInterface();
 
+            sendPlayerMove(new int[]{3, 5});
+            int[][] clientUpdatedCoords = receiveClientUpdatedCoords();
+            ui.receiveServerMessage(clientUpdatedCoords);
+
+            int[] serverMove = receiveServerMove();
+
+            ArrayList<int[]> serverUpdatedCoords = receiveServerUpdatedcoords();
+
+//            othelloPlayer.updateBoard(serverUpdatedCoords);
+//            ui.receiveServerMessage((int[][]) serverUpdatedCoords.toArray());
+
+
+
             //While the game is running
-                //Update board
-                //Have user input move
-                //Send move to server
-                //if valid, accept, wait 5 seconds, and send server's move
+            //Update board
+            //Have user input move
+            //Send move to server
+            //if valid, accept, wait 5 seconds, and send server's move
 
-                //if no more valid moves on either side
-                //procedure to end game
-                //print to client who won
-                //close connection
+            //if no more valid moves on either side
+            //procedure to end game
+            //print to client who won
+            //close connection
+        }
+        catch (IOException ex)
+        {
+            System.out.println("An error occurred during the connection: " + ex.getMessage());
+        }
+    }
 
+    private void sendPlayerMove(int[] playerMove) throws IOException
+    {
+        String moveToSend = "" + playerMove[0] + "," + playerMove[1];
+        System.out.println("Sending move: " + moveToSend);
+        writer.write(moveToSend);
+        writer.newLine();
+        writer.flush();
+    }
+
+    private int[][] receiveClientUpdatedCoords() throws IOException
+    {
+        String lastMove = reader.readLine();
+        System.out.println("Read last move: " + lastMove);
+
+        String updatedCoords = reader.readLine();
+        System.out.println("Updated coords: " + updatedCoords);
+        String[] coords = updatedCoords.split("\\|");
+        for (String coord : coords) {
+            System.out.println(coord);
+        }
+
+        ArrayList<int[]> coordinateList = new ArrayList<>();
+
+        for (String coordstr : coords) {
+            String[] nums = coordstr.split(",");
+
+            int[] coord = new int[]{
+                Integer.parseInt(nums[0]),
+            Integer.parseInt(nums[1]),
+            Integer.parseInt(nums[2]),
+};
+            coordinateList.add(coord);
+        }
+        for (int[] item : coordinateList) {
+            System.out.println(item[0] + " " + item[1] + " " + item[2]);
+        }
+
+        System.out.println(coordinateList.size());
+        int[][] result = new int[coordinateList.size()][coordinateList.getFirst().length];
+
+        for (int i = 0; i < coordinateList.size(); i++) {
+            System.out.println(i);
+            System.out.println(coordinateList.get(i).length);
+            result[i] = coordinateList.get(i);
+        }
+
+        othelloPlayer.updateBoard(coordinateList);
+
+        OthelloPlayer.printBoard(othelloPlayer.getBoard());
+
+        return result;
+    }
+
+    private int[] receiveServerMove() throws IOException
+    {
+        String move = reader.readLine();
+        System.out.println("Server move: " + move);
+
+        String[] serverCoordinate = move.split(",");
+        int[] coord = new int[] {
+                Integer.parseInt(serverCoordinate[0]),
+                Integer.parseInt(serverCoordinate[1]),
+        };
+
+        return coord;
+    }
+
+    private ArrayList<int[]> receiveServerUpdatedcoords() throws IOException
+    {
+        String updatedCoords = reader.readLine();
+        System.out.println("Updated coords: " + updatedCoords);
+
+        String[] coords = updatedCoords.split("\\|");
+        for (String coord : coords) {
+            System.out.println(coord);
+        }
+
+        ArrayList<int[]> coordinateList = new ArrayList<>();
+
+        for (String coordstr : coords) {
+            String[] nums = coordstr.split(",");
+
+            int[] coord = new int[]{
+                    Integer.parseInt(nums[0]),
+                    Integer.parseInt(nums[1]),
+                    Integer.parseInt(nums[2]),
+            };
+            coordinateList.add(coord);
+        }
+
+        return coordinateList;
     }
 }
